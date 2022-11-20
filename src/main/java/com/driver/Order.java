66fe972891967c02599b3ef57adcb628978cf6dc
BharatChaudhary102 <chaudhary.bharatbhai.28@ldce.ac.in>
2022-11-20 17:35:54 +0530

    commiting code

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,6 +9,14 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        int time = 0;
+        String hrs = deliveryTime.substring(0, 2);
+        String mins = deliveryTime.substring(3);
+        time += Integer.parseInt(hrs)*60 + Integer.parseInt(mins);
+
+        this.id = id;
+        this.deliveryTime = time;
+
     }
 
     public String getId() {
